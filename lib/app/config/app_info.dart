a86fa0b1acdf51d5202b43979f2b3a9f9905073f
lane4dev <dev@tangyu.me>
2026-01-22 16:56:37 +0800

    feat: Add package_info_plus for enhanced app information retrieval

---
+++ lib/app/config/app_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'app_info.g.dart';
+
+@riverpod
+Future<PackageInfo> packageInfo(Ref ref) {
+  return PackageInfo.fromPlatform();
+}
